08d3f299866a318501f7ac9a03aab7bd890d88af
Rifat <hrifat450@gmail.com>
2023-07-26 23:52:33 +0600

    version 1.0.4 released

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,22 @@
-import { Schema, SchemaDefinition, SchemaDefinitionType } from "mongoose";
-import { ICommon, IModel, IQueryHelpers } from "./interface";
+import { HydratedDocument, Model, QueryWithHelpers, Schema, SchemaDefinition, SchemaDefinitionType, Types } from 'mongoose';
+export interface ICommon {
+    _id: Types.ObjectId;
+    deleted_at: Date;
+}
+export interface IQueryHelpers<T> {
+    notDeleted(): QueryWithHelpers<HydratedDocument<T>, HydratedDocument<T>, IQueryHelpers<T>>;
+    onlyDeleted(): QueryWithHelpers<HydratedDocument<T>, HydratedDocument<T>, IQueryHelpers<T>>;
+    withDeleted(): QueryWithHelpers<HydratedDocument<T>, HydratedDocument<T>, IQueryHelpers<T>>;
+}
+export interface IModel<IDoc, TQueryHelpers = IQueryHelpers<IDoc>, TInstanceMethods = {}> extends Model<IDoc, TQueryHelpers, TInstanceMethods> {
+    softDeleteById(_id: Types.ObjectId): IDoc;
+    softDelete(): any;
+    restoreById(): IDoc;
+    restore(): any;
+    restoreAll(): any;
+    forceDeleteById(): IDoc;
+    forceDelete(): IDoc[];
+}
 declare class MongooseSchema<IDoc, IInstanceMethods = {}, QueryHelpers = {}> extends Schema<IDoc, IModel<IDoc, QueryHelpers, IInstanceMethods>, IInstanceMethods, IQueryHelpers<IDoc> & QueryHelpers> {
     constructor(schema: SchemaDefinition<SchemaDefinitionType<IDoc & ICommon>>, timestamps: {
         timestamps: boolean;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
-import { HydratedDocument, QueryWithHelpers, Schema, SchemaDefinition, SchemaDefinitionType, Types } from 'mongoose';
-import { ICommon, IModel, IQueryHelpers } from './interface';
+import { HydratedDocument, Model, QueryWithHelpers, Schema, SchemaDefinition, SchemaDefinitionType, Types } from 'mongoose';
+
+
+// interface for common fields
+export interface ICommon {
+  _id: Types.ObjectId;
+  deleted_at: Date;
+}
+
+// interface for query helpers
+export interface IQueryHelpers<T> {
+  notDeleted(): QueryWithHelpers<HydratedDocument<T>, HydratedDocument<T>, IQueryHelpers<T>>;
+  onlyDeleted(): QueryWithHelpers<HydratedDocument<T>, HydratedDocument<T>, IQueryHelpers<T>>;
+  withDeleted(): QueryWithHelpers<HydratedDocument<T>, HydratedDocument<T>, IQueryHelpers<T>>;
+}
+
+// interface for model
+export interface IModel<IDoc, TQueryHelpers = IQueryHelpers<IDoc>, TInstanceMethods = {}>
+  extends Model<IDoc, TQueryHelpers, TInstanceMethods> {
+  softDeleteById(_id: Types.ObjectId): IDoc;
+  softDelete(): any;
+  restoreById(): IDoc;
+  restore(): any;
+  restoreAll(): any;
+  forceDeleteById(): IDoc;
+  forceDelete(): IDoc[];
+}
 
 class MongooseSchema<IDoc, IInstanceMethods = {}, QueryHelpers = {}> extends Schema<
   IDoc,
